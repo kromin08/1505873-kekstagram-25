ee13dc348ae20cc69e21942812de8442c38047b0
kromin08 <kromin08@gmail.com>
2022-02-21 18:51:50 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,9 +5,20 @@ const COUNT_COMMENT = function (userComment, maxlength) {
   if (LENGTH_COMMENT <= maxlength) {
     return true;
   } else {
+    Error('Максимальное количество символов комментария не более 140');
     return false;
   }
 };
 COUNT_COMMENT('Комментарий посетителя сайта');
 
-// Источник:
+// Источник: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min < 0 || max < 0 || max === min || min === max || max < min || min > max) {
+    Error('Вводите целые положительные числа от меньшего к большему');
+  } else {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+}
+getRandomIntInclusive(20, 10);
