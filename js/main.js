629491c4e14798cc1f77fc2b64988703c354adc8
kromin08 <kromin08@gmail.com>
2022-03-03 18:16:58 +0300

    Удаляю дубль функцию

--- js/main.js
+++ js/main.js
@@ -1,16 +1,57 @@
 // Источник: http://kodesource.top/javascript/form/string-length.php
-const MAX_LENGHT = 140;
-const COUNT_COMMENT = function (userComment) {
-  if (userComment.length <= MAX_LENGHT) {
+const maxLenght = 140;
+const countComment = function (userComment) {
+  if (userComment.length <= maxLenght) {
     return true;
   } else {
     Error('Максимальное количество символов комментария не более 140');
     return false;
   }
 };
-COUNT_COMMENT('Комментарий посетителя сайта');
+countComment('Комментарий посетителя сайта');
+
+// 4.9 Больше деталей.
+const getNewObjects1 = (numberObjectsCreate) => {
+  const createKey = [];
+  for (let i = 0; i <= numberObjectsCreate - 1; i++) {
+    createKey[i] = {};
+    createKey[i].id = i;
+    createKey[i].url = 9;
+    createKey[i].description = 'Первый';
+    createKey[i].likes = '199';
+    createKey[i].avatar = '25';
+    createKey[i].comments = 'В целом Плохо.';
+  }
+  return createKey;
+};
+getNewObjects1(25);
+
+
+const description = ['Отель и море', 'Указатель к морю', 'Сейшеллы', 'Фотобикини',
+  'Онсен-Кари', 'МакЛарен', 'Диета', 'Морс', 'Пляж и самолёт', 'Удобство хранения', 'Тропинка к пляжу',
+  'RS5 на сликах', 'Высокая морская кухня', 'Суси из котэ', 'Домашние унты', 'Пролетая на Тибетом',
+  'Бей, а мы споём', 'Красивая классика', 'Ночь, туалет, хайтэк', 'Восточный релакс отель', 'Дорого, мало, но модно',
+  'Закат и чувства', 'Тропический краб', 'Файер шоу на битах', 'Джип-сафари'];
+const comments = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const names = ['Паша', 'Гагик', 'Эмили', 'Эльвира', 'Алёша', 'Виолетта'];
+
+let id = 1;
+
+const getRandomItenger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
 
 // Источник: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// eslint-disable-next-line no-unused-vars
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -20,58 +61,29 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 }
-getRandomIntInclusive(20, 10);
 
-// 4.9 Больше деталей.
-/*const GET_NEW_OBJECTS = (NUMBER_OBJECTS_CREATE) => {
-  const CREATE_KEY = [];
-  for (let i = 0; i <= NUMBER_OBJECTS_CREATE - 1; i++) {
-    CREATE_KEY[i] = {};
-    CREATE_KEY[i].ID = '999';
-    CREATE_KEY[i].URL = 1;
-    CREATE_KEY[i].DESCRIPTION = 'Первый';
-    CREATE_KEY[i].likes = '299';
-    CREATE_KEY[i].avatar = '25';
-    CREATE_KEY[i].COMMENTS = 'В целом Плохо.';
-  }
-  return CREATE_KEY;
-};
-GET_NEW_OBJECTS(5);
-*/
+const getRandomArrayObject = (elements) => elements[getRandomItenger(0, elements.length - 1)];
 
-const ID = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
-const URL = [];
-const DESCRIPTION = ['Отель и море','Указатель к морю','Сейшеллы','Фотобикини',
-  'Спакари','МакЛарен','Диета','Морс','Пляж и самолёт','Удобство хранения','Тропинка к пляжу',
-  'RS5 на сликах','Высокая морская кухня','Суси из котэ','Домашние унты','Пролетая на Тибетом',
-  'Бей, а мы споём','Красивая классика','Ночь, туалет, хайтэк','Восточный релакс отель','Дорого, мало, но модно',
-  'Закат и чувства','Тропический краб','Файер шоу на битах','Джип-сафари'];
-const COMMENTS = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-const NAMES = ['Паша','Гагик','Эмили','Эльвира','Алёша','Виолетта'];
+const getCommentsArray = (commentsCount) =>
+  Array.from({ length: commentsCount }, () => ({
+    id: Date.now() + Math.random(),
+    avatar: `img/avatar-${getRandomIntInclusive(1, 6)}.svg`,
+    message: getRandomArrayObject(comments),
+    name: getRandomArrayObject(names),
+  }));
 
-
-const GET_RANDOM_ITENGER = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+const newObjects = () => {
+  const currentID = id++;
+  return {
+    id: currentID,
+    url: `photos/${currentID}.jpg`,
+    likes: getRandomIntInclusive(15, 200),
+    comments: getCommentsArray(2),
+    description: getRandomArrayObject(description),
+  };
 };
 
-const GET_RANDON_ARRAY_OBJECT = (elements) => elements[GET_RANDOM_ITENGER(0, elements.length - 1)];
-
-const NEW_OBJECTS = () => ({
-  id: GET_RANDON_ARRAY_OBJECT(ID),
-  url: GET_RANDON_ARRAY_OBJECT(URL),
-  likes: GET_RANDON_ARRAY_OBJECT(LIKES),
-  description: GET_RANDON_ARRAY_OBJECT(DESCRIPTION),
-});
-
-const similarNEW_OBJECTS = Array.from({length: 2}, NEW_OBJECTS);
-similarNEW_OBJECTS();
+const getNewObjects = Array.from({ length: 25 }, newObjects);
+window.console.log(getNewObjects);
+window.console.log(Array.from({ length: 25 }, newObjects));
 
